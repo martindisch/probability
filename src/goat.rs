4653db06bc20642ce3bb08ba42f191e706ec70f9
Martin Disch <martindisch@gmail.com>
2021-03-12 18:45:12 +0100

    Implement verbose solution
    It expresses all possible states to determine which door to open and
    which one will be finally chosen by the contestant.

--- src/goat.rs
+++ src/goat.rs
@@ -1,6 +1,91 @@
 use rand::prelude::*;
 
-pub fn run() {}
+pub fn run() {
+    let mut test_runner = TestRunner::new(vec![
+        Box::from(NonSwitchingContestant),
+        Box::from(SwitchingContestant),
+    ]);
+
+    let results = test_runner.compare_strategies(1_000_000);
+    println!("{:#?}", results);
+}
+
+struct TestRunner {
+    contestants: Vec<Box<dyn Contestant>>,
+}
+
+impl TestRunner {
+    /// Initializes a new `Testrunner`.
+    fn new(contestants: Vec<Box<dyn Contestant>>) -> Self {
+        Self { contestants }
+    }
+
+    /// Compares the contestant strategies against each other.
+    ///
+    /// # Arguments
+    /// * `runs` - The number of runs to do.
+    fn compare_strategies(&mut self, runs: u32) -> Vec<TestResult> {
+        let mut wins: Vec<u32> = vec![0; self.contestants.len()];
+
+        for _ in 0..runs {
+            let original_stage = Stage::new();
+
+            for (i, contestant) in self.contestants.iter().enumerate() {
+                let mut stage = original_stage;
+
+                // Let contestant make their first choice
+                let first_choice = contestant.first_choice();
+
+                // Open another door containing a goat
+                let door_to_open = match first_choice {
+                    0 if stage.doors[1] == Door::Closed(Content::Goat) => 1,
+                    0 => 2,
+                    1 if stage.doors[0] == Door::Closed(Content::Goat) => 0,
+                    1 => 2,
+                    2 if stage.doors[0] == Door::Closed(Content::Goat) => 0,
+                    2 => 1,
+                    _ => unreachable!(),
+                };
+                stage.doors[door_to_open] = Door::Open(Content::Goat);
+
+                // Let contestant make their final choice
+                let final_choice = match (
+                    contestant.switch_door(),
+                    first_choice,
+                    door_to_open,
+                ) {
+                    (false, _, _) => first_choice,
+                    (true, 0, 1) => 2,
+                    (true, 0, 2) => 1,
+                    (true, 1, 0) => 2,
+                    (true, 1, 2) => 0,
+                    (true, 2, 0) => 1,
+                    (true, 2, 1) => 0,
+                    _ => unreachable!(),
+                };
+
+                // Determine result
+                if stage.doors[final_choice] == Door::Closed(Content::Car) {
+                    wins[i] += 1;
+                }
+            }
+        }
+
+        wins.iter()
+            .enumerate()
+            .map(|(i, &total)| TestResult {
+                contestant_strategy: self.contestants[i].name(),
+                number_of_wins: total,
+            })
+            .collect()
+    }
+}
+
+#[derive(Debug)]
+struct TestResult {
+    contestant_strategy: &'static str,
+    number_of_wins: u32,
+}
 
 /// The stage with 3 doors.
 #[derive(Clone, Copy)]
@@ -20,14 +105,14 @@ impl Stage {
 }
 
 /// A door on the stage.
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, PartialEq)]
 enum Door {
     Closed(Content),
     Open(Content),
 }
 
 /// The content behind a door.
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, PartialEq)]
 enum Content {
     Goat,
     Car,
